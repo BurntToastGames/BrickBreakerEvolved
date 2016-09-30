0c21eff4a2ab37e718d9f383e4f73845768adad8
Monish Gupta <monish1478@gmail.com>
2016-09-30 17:41:58 -0600

    Pending blocks works for both players! Huzzah!

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -5,6 +5,17 @@ public class Brick : MonoBehaviour
 {
 	void OnCollisionEnter2D(Collision2D col)
     {
+        GameObject brickBlob = this.transform.parent.gameObject;
+
+        if(brickBlob.tag == "Bricks1")
+        {
+            GameObject.FindGameObjectWithTag("Game Manager").SendMessage("sendBricks", 1);
+        }
+        else
+        {
+            GameObject.FindGameObjectWithTag("Game Manager").SendMessage("sendBricks", 2);
+        }
+
         Destroy(this.gameObject);
     }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public Player player1, player2;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player1 = new Player()
+        {
+            brickCount = GameObject.FindGameObjectWithTag("Bricks1").transform.childCount,
+            pendingBricks = 0,
+            Paddle = GameObject.FindGameObjectWithTag("Paddle1"),
+            Ball = GameObject.FindGameObjectWithTag("Ball1")
+        };
+
+        player2 = new Player()
+        {
+            brickCount = GameObject.FindGameObjectWithTag("Bricks2").transform.childCount,
+            pendingBricks = 0,
+            Paddle = GameObject.FindGameObjectWithTag("Paddle2"),
+            Ball = GameObject.FindGameObjectWithTag("Ball2")
+        };
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+
+	}
+
+    void sendBricks(int player)
+    {
+
+        Player temp = player == 1 ? player1 : player2;
+
+        temp.pendingBricks++;
+        temp.brickCount--;
+
+        print("Player" + player + " : " + temp.pendingBricks + " pending");
+    }
+
+}
+
+
+public class Player
+{
+    public int brickCount { get; set; }
+
+    public int pendingBricks { get; set; }
+
+    public GameObject Paddle { get; set; }
+    public GameObject Ball { get; set; }
+
+    public Player()
+    { }
+}
