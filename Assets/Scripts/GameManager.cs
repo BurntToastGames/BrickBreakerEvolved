41a9ffef517d6bacf6c2dd3e00dcfcdf88ffc403
Monish Gupta <monish1478@gmail.com>
2016-10-02 02:28:51 -0600

    So close to adding lines!
    Why do you do this local and world positions?!

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,13 @@ public class GameManager : MonoBehaviour {
 
     public Player player1, player2;
 
+    public GameObject LinePrefab;
+
+    private int bricksPerLine = 12;
+    private float lineSpaceConst = 1.14f;
+
+    
+
 	// Use this for initialization
 	void Start ()
     {
@@ -14,7 +21,8 @@ public class GameManager : MonoBehaviour {
             brickCount = brickCountHelper(GameObject.FindGameObjectWithTag("Bricks1")),
             pendingBricks = 0,
             Paddle = GameObject.FindGameObjectWithTag("Paddle1"),
-            Ball = GameObject.FindGameObjectWithTag("Ball1")
+            Ball = GameObject.FindGameObjectWithTag("Ball1"),
+            recentlyAddedLineY = 0
         };
 
         player2 = new Player()
@@ -23,10 +31,10 @@ public class GameManager : MonoBehaviour {
             brickCount = brickCountHelper(GameObject.FindGameObjectWithTag("Bricks2")),
             pendingBricks = 0,
             Paddle = GameObject.FindGameObjectWithTag("Paddle2"),
-            Ball = GameObject.FindGameObjectWithTag("Ball2")
+            Ball = GameObject.FindGameObjectWithTag("Ball2"),
+            recentlyAddedLineY = 0
         };
     }
-
     int brickCountHelper(GameObject brickGroup)
     {
         int brickCount = 0;
@@ -45,16 +53,35 @@ public class GameManager : MonoBehaviour {
 
 	}
 
+
     void sendBricks(int player)
     {
-        Player temp = player == 1 ? player1 : player2;
+        Player tempPlayer = player == 1 ? player1 : player2;
+        Player victim = player == 2 ? player1 : player2;
 
-        temp.pendingBricks++;
-        temp.brickCount--;
+        tempPlayer.pendingBricks++;
+        tempPlayer.brickCount--;
 
-        print("Player" + player + " : " + temp.pendingBricks + " pending");
+        if(tempPlayer.pendingBricks == bricksPerLine)
+        {
+            tempPlayer.pendingBricks -= bricksPerLine;
+
+            Vector3 newBrickGroupPosition = new Vector3(victim.BrickGroup.transform.position.x,
+                                                        victim.BrickGroup.transform.position.y - (lineSpaceConst * victim.BrickGroup.transform.localScale.y));
+
+            victim.BrickGroup.transform.position = newBrickGroupPosition;
+
+            Vector3 newLinePositionWithinParent = new Vector3(0, victim.recentlyAddedLineY + lineSpaceConst);
+            victim.recentlyAddedLineY += lineSpaceConst;
+
+            GameObject newLine = Instantiate(LinePrefab, victim.BrickGroup.transform.position, Quaternion.identity) as GameObject;
+            newLine.transform.parent = victim.BrickGroup.transform;
+            newLine.transform.localPosition = newLinePositionWithinParent;
+            newLine.transform.localScale = Vector3.one;
+        }
+
+        print("Player" + player + " : " + tempPlayer.pendingBricks + " pending");
     }
-
 }
 
 
@@ -67,6 +94,8 @@ public class Player
     public GameObject BrickGroup { get; set; }
     public GameObject Paddle { get; set; }
     public GameObject Ball { get; set; }
+
+    internal float recentlyAddedLineY;
 
     public Player()
     { }
